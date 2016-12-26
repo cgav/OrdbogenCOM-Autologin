2b398558a7aa7416f6d262fa9429d1d6fcf4bff2
Constantin Gavrilete <cg@lix.tech>
2016-12-26 21:13:30 +0100

    adds auto login feature

--- src/background.js
+++ src/background.js
@@ -1,1 +1,43 @@
-console.log('background loaded!');
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const UNILOGIN_URL = 'https://login.emu.dk';
+
+const login = async () => {
+  const result = await axios.get(UNILOGIN_URL);
+  const $ = cheerio.load(result.data);
+
+  // fetching all those params from site
+  const selectionResults = $('#pwd input').toArray();
+  if (selectionResults.length === 0) {
+    console.log('Already logged in');
+    return;
+  }
+  let inputs = selectionResults.reduce((acc, input) => {
+    const $input = $(input);
+    acc[$input.attr('name')] = $input.attr('value');
+    return acc;
+  }, {});
+  
+  // injecting custom inputs
+  inputs.user = UNILOGIN_USER;
+  inputs.pass = UNILOGIN_PASS;
+  inputs.nypass1 = "";
+  inputs.nypass2 = "";
+
+  delete inputs.undefined;
+  delete inputs.setpass;
+  
+  let params = new URLSearchParams();
+  Object.keys(inputs).forEach(i => params.append(i, inputs[i]));
+  const loginResult = await axios.post(UNILOGIN_URL, params);
+};
+
+chrome.runtime.onMessage.addListener(({ type }, sender, responseFn ) => {
+  if (type === 'LOG_ME_IN') {
+    login()
+      .then(() => responseFn({ error: null }))
+      .catch((error) => responseFn({ error }));
+  }
+  return true;
+});
--- src/content.js
+++ src/content.js
@@ -1,1 +1,18 @@
-console.log('content loaded!');
+(() => {
+
+  const button = document.getElementById('loginButton');
+  if (!button) {
+    return;
+  }
+
+  if (button.textContent.trim() === 'Log ind') {
+    chrome.runtime.sendMessage({ type: 'LOG_ME_IN' }, (response) => {
+      if (response.error) {
+        console.error(response.error);
+        return;
+      }
+      window.location.href = "http://www.ordbogen.com/singlesignon/unilogin.php";
+    });
+  }
+
+})();
