65f889a6e68c61b5928c9fa0026d61d0fe730be2
Constantin Gavrilete <cg@lix.tech>
2016-12-26 19:28:18 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const envs = require('./envs.json');
+
+module.exports = {
+  entry: {
+    popup: ['babel-polyfill', './src/popup.js'],
+    background: ['babel-polyfill', './src/background.js'],
+    content: ['babel-polyfill', './src/content.js']
+  },
+  output: {
+    path: './build',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /.json$/, loader: "json-loader" }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'UNILOGIN_USER': JSON.stringify(envs.UNILOGIN_USER),
+      'UNILOGIN_PASS': JSON.stringify(envs.UNILOGIN_PASS)
+    })
+  ]
+};
